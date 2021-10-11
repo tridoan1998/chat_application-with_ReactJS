f3da4c20a126c9cad24b616e44305ac69ba93ecd
Tri Doan <tridoan1998@gmail.com>
2021-10-11 00:50:39 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return (
+        < ChatEngine
+            height = "100"
+            projectID = "8c193b84-ba08-48bc-a887-79cafdbf99d1"
+            userName= "javascriptmastery"
+            userSecret = "123123"
+        />
+    )
+
+}
+
+export default App; 
