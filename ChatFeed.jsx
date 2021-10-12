140bdaa8cd3eae831cab815d5ab1c0014c598a5a
Tri Doan <tridoan1998@gmail.com>
2021-10-11 17:13:04 -0700

    Add files via upload

---
+++ ChatFeed.jsx
@@ -0,0 +1,45 @@
+
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map(key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index == 0 ? null : keys[index - 1];
+            const isMyMessage = username == message.sender.userName;
+
+            return(
+                <div key= {`msg_${index}`} style={{ width: '100%' }}>
+                        <div className="message-block">
+                            {
+                                isMyMessage
+                                ? <MyMessage />
+                                : <TheirMessage />
+                            }
+                            </div>
+                        <div className="read-receipts" style={{marginRight: isMyMessage ? 18fx : '0fx', }} > </div>
+                    </div>
+            )
+        }
+
+    }
+
+    renderMessages()
+
+    return (
+            <div>
+                    ChatFeed
+            </div>
+
+    );
+}
+
+export default ChatFeed;
